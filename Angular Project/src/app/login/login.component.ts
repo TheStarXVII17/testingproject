721f049c5d343b3372a0bd0a45bcd2ca87afc18f
Caster Troy <bluexephosalmeda@gmail.com>
2019-08-31 06:19:41 +0800

    git commit -a

---
+++ Angular Project/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  gi = '';
+  ni = '';
+  ang = '';
+  ni2 = '';
+
+  ngOnInit() {
+  }
+
+}
